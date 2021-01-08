e09c462ec7a6b333eca36d5cd2f4704244f9937f
Tao Hansen <taohansens@gmail.com>
2021-01-08 18:57:03 -0300

    Show map error

---
+++ front-web/src/Orders/OrderLocation.tsx
@@ -0,0 +1,32 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+const position = { 
+    lat: -9.016240437656453,
+    lng: -42.689694189602335
+}
+
+function OrderLocation() {
+  return (
+    <div className="order-location-container">
+      <div className="order-location-content">
+        <h3 className="order-location-title">
+          Selecione onde o pedido deve ser entregue:
+        </h3>
+        <div className="filter-container">
+          <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default OrderLocation;
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
@@ -17,6 +18,7 @@ function Orders(){
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products}/>
+            <OrderLocation />
         </div>
     )
 }
