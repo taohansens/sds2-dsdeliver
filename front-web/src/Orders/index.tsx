0f35541ea931273dd5d9dd766c3617ba30fe91be
Tao Hansen <taohansens@gmail.com>
2021-01-09 18:06:35 -0300

    Selected Products. FormatPrice and Sum in OrdersSummary

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrdersSummary';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
@@ -8,7 +11,11 @@ import { OrderLocationData, Product } from './types';
 
 function Orders(){
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price;
+    }, 0)
 
     useEffect(() => {
         fetchProducts()
@@ -16,12 +23,36 @@ function Orders(){
             .catch(error => console.log(error))
     }, []);
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product);
+
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return (
+        <>
         <div className="orders-container">
             <StepsHeader />
-            <ProductsList products={products}/>
-            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
+            <ProductsList
+                products={products}
+                onSelectProduct={handleSelectProduct}
+                selectedProducts={selectedProducts}
+            />
+            <OrderLocation
+                onChangeLocation= {location => setOrderLocation(location)}
+            />
+            <OrderSummary 
+                amount={selectedProducts.length}
+                totalPrice={totalPrice}
+            />
         </div>
+        <Footer />
+        </>
     )
 }
 
