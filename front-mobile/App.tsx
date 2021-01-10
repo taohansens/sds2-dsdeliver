9db810f928191c7ce4c84cb7c9d366c8c6fdf971
Tao Hansen <taohansens@gmail.com>
2021-01-10 13:28:51 -0300

    Routes. OrdersPage init

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 // Fontes (https://docs.expo.io/guides/using-custom-fonts/#using-a-google-font)
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import AppLoading from 'expo-app-loading';
-import Header from './src/Header';
-import Home from './src/Home';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -19,9 +18,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Routes />
       <StatusBar style="light" />
-      <Header />
-      <Home />
     </View>
   );
 }
---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import Header from '../Header';
+
+function Orders() {
+
+  return (
+    <View>
+      <Header />
+      <Text>Listagem de Pedidos</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default Orders;
